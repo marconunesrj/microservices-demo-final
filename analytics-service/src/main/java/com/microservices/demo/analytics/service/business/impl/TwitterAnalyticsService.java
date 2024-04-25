4ac2b051b0e29ff69a967936f36e1b614c5650ce
marconunesrj <marconunes.rj@hotmail.com>
2024-04-25 17:04:05 -0300

    Servico analytics implementado

---
+++ analytics-service/src/main/java/com/microservices/demo/analytics/service/business/impl/TwitterAnalyticsService.java
@@ -0,0 +1,33 @@
+package com.microservices.demo.analytics.service.business.impl;
+
+import com.microservices.demo.analytics.service.business.AnalyticsService;
+import com.microservices.demo.analytics.service.dataaccess.repository.AnalyticsRepository;
+import com.microservices.demo.analytics.service.model.AnalyticsResponseModel;
+import com.microservices.demo.analytics.service.transformer.EntityToResponseModelTransformer;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TwitterAnalyticsService implements AnalyticsService {
+
+    private final AnalyticsRepository analyticsRepository;
+
+    private final EntityToResponseModelTransformer entityToResponseModelTransformer;
+
+    public TwitterAnalyticsService(AnalyticsRepository repository,
+                                   EntityToResponseModelTransformer transformer) {
+        this.analyticsRepository = repository;
+        this.entityToResponseModelTransformer = transformer;
+    }
+
+    @Override
+    public Optional<AnalyticsResponseModel> getWordAnalytics(String word) {
+        // PageRequest.of(0, 1) -> para obter o primeiro registro, que será a última contagem de palavras para uma palavra,
+        // conforme consultamos usando ordem por data de criação.
+        return entityToResponseModelTransformer.getResponseModel(
+                analyticsRepository.getAnalyticsEntitiesByWord(word, PageRequest.of(0, 1))
+                        .stream().findFirst().orElse(null));
+    }
+}
